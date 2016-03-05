a96cd34da8e7975887a293aa9ee29c66330ddc67
Michaël van de Weerd <com@michaelvandeweerd.net>
2016-03-05 13:40:46 +0100

    Added the Script content for future use, not yet implemented

---
+++ src/eu/parcifal/intra/content/Script.java
@@ -0,0 +1,11 @@
+package eu.parcifal.intra.content;
+
+public class Script extends Content {
+
+	private String location;
+	
+	public Script(String location) {
+		this.location = location;
+	}
+	
+}
